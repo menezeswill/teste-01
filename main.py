cbcb9b745d0f3de5003e9ec6822c74dd45085615
William <menezeswill@gmail.com>
2024-10-17 12:57:17 -0300

    Primeiro codigo

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+
+# Loop
+for x in range(10):
+    print(x+1)
+
+# Variaveis
+idade = 34
+nome = "Luciano"
+
+print(idade)
+
+# Soma
+nova_idade = idade +1
+print (nova_idade)
+
+# Listas
+lista=[1,2,3,4,5]
+
+# Metodos
+lista_invertida=lista.reverse()
+
+print (lista_invertida)
+
